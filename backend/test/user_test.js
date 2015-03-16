3d2fbe9fc44c3fd912a32791b66771add9388b27
Mikko Lehtinen <m@mikkolehtinen.com>
2015-03-16 11:07:37 +0200

    Update user_test.js

--- backend/test/user_test.js
+++ backend/test/user_test.js
@@ -5,19 +5,6 @@ var should = require('chai').should();
 describe('User tests', function() {
   var user;
 
-  it('should add roles to User', function() {
-    var usr = new models.User();
-    usr.addRoles('a');
-    usr.get('roles').length.should.equal(1);
-    usr.get('roles').should.contain('a');
-    usr.addRoles('a', 'b');
-    usr.get('roles').length.should.equal(2);
-    usr.get('roles').should.contain('b');
-    usr.addRoles(['c', 'd']);
-    usr.get('roles').length.should.equal(4);
-    usr.get('roles').should.contain('d');
-  });
-
   it('should save a User', function() {
     user = new models.User({
       username: 'foouser',
